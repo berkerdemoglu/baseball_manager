266c45151c4b7113722207306194c672ea9bec91
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-04-17 16:12:39 +0300

    Add bootstrap and styling to some files

--- baseball_manager/settings.py
+++ baseball_manager/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     # Third party apps
     'django_countries',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
